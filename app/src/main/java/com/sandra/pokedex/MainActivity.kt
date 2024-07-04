cc2cf958a301d0891b256c577af808c1f7012fb7
IGLIKOWSKI <iglikowskicristiani@gmail.com>
2024-07-04 13:29:15 -0300

    ajustes screen two

--- app/src/main/java/com/sandra/pokedex/MainActivity.kt
+++ app/src/main/java/com/sandra/pokedex/MainActivity.kt
@@ -9,6 +9,6 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.welcome_screen_two)
     }
 }
