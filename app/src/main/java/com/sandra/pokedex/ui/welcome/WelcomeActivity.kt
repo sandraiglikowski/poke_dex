7d3b7667cafb93783ed37a094018b917e19488e6
IGLIKOWSKI <iglikowskicristiani@gmail.com>
2024-07-08 14:26:17 -0300

    com erro kk

---
+++ app/src/main/java/com/sandra/pokedex/ui/welcome/WelcomeActivity.kt
@@ -0,0 +1,13 @@
+package com.sandra.pokedex.ui.welcome
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WelcomeActivity : AppCompatActivity() {
+
+    private var binding: Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/sandra/pokedex/ui/welcome/WelcomeOneFragment.kt
@@ -0,0 +1,7 @@
+package com.sandra.pokedex.ui.welcome
+
+import androidx.fragment.app.Fragment
+import com.sandra.pokedex.R
+
+class WelcomeOneFragment : Fragment(R.layout.welcome_screen_one_fragment) {
+}
