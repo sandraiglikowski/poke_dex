1a1f6b8ee9371bf9dff26b97a21ba2491e198c85
IGLIKOWSKI <iglikowskicristiani@gmail.com>
2024-07-13 21:54:52 -0300

    tela de cadastro one

--- app/src/main/java/com/sandra/pokedex/MainActivity.kt
+++ app/src/main/java/com/sandra/pokedex/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.splash_screen)
+        setContentView(R.layout.register_one)
         
     }
 }
